005dfd35239a60777d518b52906e3d6053bb339b
Carlos Henrique <ichfs9108@gmail.com>
2023-09-27 23:32:22 -0300

    Checklist do Spring Security

--- src/main/java/com/devsuperior/empregados/auth/entities/Role.java
+++ src/main/java/com/devsuperior/empregados/auth/entities/Role.java
@@ -1,11 +1,13 @@
 package com.devsuperior.empregados.auth.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +30,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
--- src/main/java/com/devsuperior/empregados/auth/entities/User.java
+++ src/main/java/com/devsuperior/empregados/auth/entities/User.java
@@ -1,13 +1,17 @@
 package com.devsuperior.empregados.auth.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -58,6 +62,7 @@ public class User {
         this.email = email;
     }
 
+
     public void setPassword(String password) {
         this.password = password;
     }
@@ -92,5 +97,40 @@ public class User {
     @Override
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
